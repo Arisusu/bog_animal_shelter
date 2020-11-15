d1f2ddba2dc5cdbe66c11e36fc5566e6978747da
Evan Montoya <you@example.com>
2020-11-15 15:14:52 -0500

    added schema

---
+++ backend/Database_Setup.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const petSchema = new Schema ({
+    name: String,
+    breed: String,
+    status: String,
+    gender: String,
+    yearsOld: Number,
+    adopted: Boolean,
+    _id: String,
+});
+
+const conn = await mongoose.createConnection('mongodb://localhost/animal_shelter');
+const Pet = conn.model('Pet', petSchema);
+const pet1 = new Pet({
+    name: 'Rocky',
+	breed: 'American pit bull cross',
+	status: 'Neutered and vaccinated',
+	gender: 'Female',
+	yearsOld: 7,
+	adopted: false,
+    _id: 'dog'
+});
+
+const pet2 = new Pet({
+    name: 'Juju',
+    breed: 'American staff cross',
+    status: 'Vaccindated, not neutered',
+    gender: 'Male',
+    yearsOld: 7,
+    adopted: true,
+    _id: 'dog'
+});
+
+const pet3 = new Pet({
+    name: 'Terry',
+    breed: 'Domestic medium hair cross',
+    status: 'Neutered, not vaccinated',
+    gender: 'Male',
+    yearsOld: 2,
+    adopted: false,
+    _id: 'cat'
+});
+
+const pet4 = new Pet({
+    name: 'Chonker',
+    breed: 'Main coone',
+    status: 'Status unknown',
+    gender: 'Female',
+    yearsOld: 5,
+    adopted: true,
+    _id: 'cat'
+});
+
+Pet.insertMany([pet1, pet2, pet3, pet4], function(err){
+    console.log(err)
+})
+
