fa6d2d05cb001c066651a9dea7f812b07c3eea5e
Alice Zeng <azeng30@gatech.edu>
2020-11-19 18:38:33 -0500

    fix database insert and add initial route

--- backend/Database_Setup.js
+++ backend/Database_Setup.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const petSchema = new Schema ({
+const petSchema = new Schema({
     name: String,
     breed: String,
     status: String,
@@ -11,49 +11,67 @@ const petSchema = new Schema ({
     id: String,
 });
 
-const conn = mongoose.createConnection('mongodb://localhost/animal_shelter', { useNewUrlParser: true });
-const Pet = conn.model('Pet', petSchema);
-const pet1 = new Pet({
-    name: 'Rocky',
-	breed: 'American pit bull cross',
-	status: 'Neutered and vaccinated',
-	gender: 'Female',
-	yearsOld: 7,
-	adopted: false,
-    id: 'dog'
+
+module.exports = new Promise((resolve, reject) => {
+    const conn = mongoose.createConnection('mongodb://localhost/animal_shelter', { useNewUrlParser: true });
+    const Pet = conn.model('Pet', petSchema);
+
+    Pet.deleteMany({}, (err) => {
+        if (err == null) {
+            console.log("Sucessfully cleared database");
+        } else {
+            console.log("Failed to clear database");
+            console.log(err);
+        }
+    });
+
+    const pet1 = new Pet({
+        name: 'Rocky',
+        breed: 'American pit bull cross',
+        status: 'Neutered and vaccinated',
+        gender: 'Female',
+        yearsOld: 7,
+        adopted: false,
+        id: 'dog'
+    });
+
+    const pet2 = new Pet({
+        name: 'Juju',
+        breed: 'American staff cross',
+        status: 'Vaccindated, not neutered',
+        gender: 'Male',
+        yearsOld: 7,
+        adopted: true,
+        id: 'dog'
+    });
+
+    const pet3 = new Pet({
+        name: 'Terry',
+        breed: 'Domestic medium hair cross',
+        status: 'Neutered, not vaccinated',
+        gender: 'Male',
+        yearsOld: 2,
+        adopted: false,
+        id: 'cat'
+    });
+
+    const pet4 = new Pet({
+        name: 'Chonker',
+        breed: 'Main coone',
+        status: 'Status unknown',
+        gender: 'Female',
+        yearsOld: 5,
+        adopted: true,
+        id: 'cat'
+    });
+
+    Pet.insertMany([pet1, pet2, pet3, pet4], function (err) {
+        if (err == null) {
+            console.log("Sucessfully inserted pets into database");
+        } else {
+            console.log("Failed to insert pets into database");
+            console.log(err);
+        }
+    });
+    resolve(Pet);
 });
-
-const pet2 = new Pet({
-    name: 'Juju',
-    breed: 'American staff cross',
-    status: 'Vaccindated, not neutered',
-    gender: 'Male',
-    yearsOld: 7,
-    adopted: true,
-    id: 'dog'
-});
-
-const pet3 = new Pet({
-    name: 'Terry',
-    breed: 'Domestic medium hair cross',
-    status: 'Neutered, not vaccinated',
-    gender: 'Male',
-    yearsOld: 2,
-    adopted: false,
-    id: 'cat'
-});
-
-const pet4 = new Pet({
-    name: 'Chonker',
-    breed: 'Main coone',
-    status: 'Status unknown',
-    gender: 'Female',
-    yearsOld: 5,
-    adopted: true,
-    id: 'cat'
-});
-
-Pet.insertMany([pet1, pet2, pet3, pet4], function(err){
-    console.log(err)
-})
-
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose");
+const database = require("../Database_Setup");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/animals', function (req, res, next) {
+  database.then(function (Pet) {
+    Pet.aggregate([
+      {
+        $group: {
+          _id: "$id",
+          "pets": {
+            "$addToSet": {
+              "name": "$name",
+              "breed": "$breed",
+              "status": "$status",
+              "gender": "$gender",
+              "yearsOld": "$yearsOld",
+              "adopted": "$adopted"
+            }
+          }
+        }
+      }
+    ]).then(aggregate => {
+      res.json(aggregate);
+    });
+  });
 });
 
 module.exports = router;
