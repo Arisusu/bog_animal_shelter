bcf22538aab9254eb25b367be3f28562f6f1b9bd
Ila Sharma <isharma37@gatech.edu>
2020-11-24 18:35:59 -0800

    fix spacing in nav bar

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
             <img src="https://image.freepik.com/free-vector/dog-cat-logo-template-veterinary_56473-127.jpg" alt="Site Logo" width="200" height="200"></img>
             <h1>Bog's Animal Shelter</h1>
             <button id = "push">About</button>
-            <button>Adopt a pet!</button>
+            <button id = "push2">Adopt a pet!</button>
         </nav>
     );
 }
