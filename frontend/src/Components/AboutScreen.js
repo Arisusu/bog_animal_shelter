26f3924f0757ca39ed829b24816f13928368e9d2
carlocasas99 <carlocasas1818@gmail.com>
2020-11-27 17:18:59 -0500

    adds simple page for each animal

--- frontend/src/Components/AboutScreen.js
+++ frontend/src/Components/AboutScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const AboutScreen = () => {
     const { desc } = useParams();
-
+    const [info, setInfo] = React.useState({});
+    async function fetchData() {
+        const response = await fetch(desc);
+        const data = await response.json();
+        setInfo(data[0]);
+    }
+    React.useEffect(() => {
+        fetchData();
+        }, []);
     return (
         <div>
-            <h3>{desc}</h3>
+        <img src="https://picsum.photos/350/200" alt="A really cute lil animal."/>
+        <h2>Name: {info.name}</h2>
+        <h2>Breed: {info.breed}</h2>
+        <h2>Status: {info.status}</h2>
+        <h2>Gender: {info.gender}</h2>
+        <h2>Age: {info.yearsOld}</h2>
+        <Link to="/">Back to Adopt Page</Link>
         </div>
     )
 }
